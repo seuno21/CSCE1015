0bd8da36368b4acb8e25f1e17c49d9853abf18e8
Okuneye <ogo0020@cell08-cse.unt.ad.unt.edu>
2025-11-14 01:42:11 -0600

    Added hello.py for VSCode assignment

---
+++ hello.py
@@ -0,0 +1,21 @@
+euid = "ogo0020"
+name = "Oluwaseun"
+print(f"My EUID is {euid} and my name is {name}.")
+
+# Which AI product do you use or like the most (e.g., ChatGPT, Copilot), and why?
+AI_product = "Copilot"
+Reason = "it helps me solve programming problems step-by-step and explains things clearly."
+print(f"My favorite AI product is {AI_product}, and {Reason}.")
+euid = "ogo0020"
+name = "Oluwaseun"
+print(f"My EUID is {euid} and my name is {name}.")
+
+# Which AI product do you use or like the most (e.g., ChatGPT, Copilot), and why?
+AI_product = "Copilot"
+Reason = "it helps me troubleshoot code and shop smarter"
+print(f"My favorite AI product is {AI_product}, and {Reason}.")
+
+
+
+
+
